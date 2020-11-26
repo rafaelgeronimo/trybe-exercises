8d52f24d0a49952398b4307c10126ae4cb1256c5
Rafael Geronimo <rafaelgeronimo@outlook.com>
2020-11-26 15:37:37 -0300

    Exercicios 3 e 4 - Done

--- exercises/bloco_4/4.2/exercicios.js
+++ exercises/bloco_4/4.2/exercicios.js
@@ -11,3 +11,14 @@ for(let i = 0; i < numbers.length; i++){
  resultado += numbers[i];
 }
 console.log('A soma de todos os número é: ' + resultado);
+/** 3. Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+ * A média aritmética é o resultado da soma de todoa os elementos dividos pelo numero total de elementos;
+ */
+let media = resultado/numbers.length;
+console.log('A média aritmética é: ' + media);
+/** 4. Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: 'valor maior que 20'. Caso não seja, imprima a mensagem: 'valor menor ou igual a 20' */
+if (media > 20){
+  console.log("Valor maior que 20");
+} else {
+  console.log("Valor menor ou igual a 20");
+}
