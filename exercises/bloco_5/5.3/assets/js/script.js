1a685f82160d6cf4a5dee610144db526fd3e7d63
Rafael Geronimo <rafaelgeronimo@outlook.com>
2020-12-06 14:49:33 -0300

    Exercise 3 - Done

--- exercises/bloco_5/5.3/assets/js/script.js
+++ exercises/bloco_5/5.3/assets/js/script.js
@@ -1,3 +1,9 @@
+window.onload = function() {
+  createDaysOfTheWeek();
+  createButton('btn-holiday', 'Feriados', 'showHolidays'); // Primeiro parâmetro: ID | Segundo parâmetro: Texto do botão | Terceiro parâmetro: action
+  createButton('btn-friday', 'Sexta-feira'); // Primeiro parâmetro: ID | Segundo parâmetro: Texto do botão
+}
+
 function createDaysOfTheWeek() {
   const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
   const weekDaysList = document.querySelector('.week-days');
@@ -10,9 +16,6 @@ function createDaysOfTheWeek() {
     weekDaysList.appendChild(dayListItem);
   };
 };
-
-createDaysOfTheWeek();
-
 // Escreva seu código abaixo.
 const days = document.querySelector('#days');
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
@@ -35,16 +38,34 @@ for (let index = 0; index < dezDaysList.length; index += 1) {
   days.appendChild(newDay);
 }
 
-function createButtonHolidays(feriados) {
+function createButton(buttonId, buttonName, action) {
   let buttonsContainer = document.querySelector('.buttons-container');
-  let createButtonHolidays = document.createElement('button');
-  createButtonHolidays.setAttribute('id', 'btn-holiday');
-  createButtonHolidays.innerText = feriados;
-  buttonsContainer.appendChild(createButtonHolidays);
+  let createButton = document.createElement('button');
+  createButton.setAttribute('id', buttonId);
+  createButton.innerText = buttonName;
+  createButton.addEventListener('click', function() {
+    activateAction(action);
+  });
+  buttonsContainer.appendChild(createButton);
 }
 
-createButtonHolidays('Feriados');
 
-function showHolidays() {
-  
+
+let checkHolidays = true;
+function activateAction(action) {
+  if (action === 'showHolidays') {
+    let holidays = document.querySelectorAll('.holiday');
+    for (let index = 0; index < holidays.length; index += 1) {
+      if (checkHolidays === true) {        
+        holidays[index].setAttribute('style', 'background-color: white;');
+      } else {
+        holidays[index].setAttribute('style', 'background-color: rgb(238,238,238);');
+      }
+    }
+    if (checkHolidays === true) {
+      checkHolidays = false;
+    } else {
+      checkHolidays = true;
+    }
+  }
 }
