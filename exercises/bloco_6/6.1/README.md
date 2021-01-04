38294334eaab6e07bd99f37ae554bb3e8b58f906
Rafael <rafaelgeronimo@outlook.com>
2021-01-04 19:39:18 -0300

    exercicio em andamento

---
+++ exercises/bloco_6/6.1/README.md
@@ -0,0 +1,40 @@
+<!-- Dia 6.1 | HTML & CSS - Forms -->
+## Dia 6.1 | HTML & CSS - Forms
+
+Nessa aula, aprendemos sobre formulários em <strong>HTML</strong>, que são uma forma de enviar informações para outra páigna do seu site ou para um servidor.
+
+Após essa aula, somos capazes de:
+- Criar formulários em <strong>HTML</strong> com as tags: `input`, `button`, `textarea`, `select`, `form`.
+
+## Exercícios
+
+### Criando um formulário de currículo
+
+- Crie um arquivo **HTML** chamado `form.html` para o formulário. -> [Acesse aqui](form.html)
+- Caso julgue necessário, crie estilos **CSS** para o seu formulário, de acordo com a sua imaginação. Coloque-os em um arquivo `styles.css`
+- Crie também um arquivo `script.js` para seu código **JavaScript**
+
+Vamos criar um formulário de cadastro de currículo com base na especificação a seguir:
+1. Crie um `<fieldset>` para os seguintes dados pessoais:
+  - Nome - Texto
+    - Limite de 40 caracteres
+    - Campo obrigatório
+  - Email - Texto
+    - Limite de 50 caracteres
+    - Campo obrigatório
+  - CPF - Text
+    - Limite de 11 caracteres
+    - Campo obrigatório
+  - Endereço - Texto
+    - Limite de 200 caracteres
+    - Campo obrigatório
+  - Cidade - Texto
+    - Limite de 28 caracteres
+    - Campo obrigatório
+  - Estado - ComboBox
+    - Todos os estados do Brasil
+    - Utilize estruturas de repetição via **JavaScript** para gerar os `<option>`
+    - Campo obrigatório
+  - Tipo - Radio Button
+    - Casa, Apartamento
+    - Campo obrigatório
---
+++ exercises/bloco_6/6.1/assets/js/script.js
@@ -0,0 +1,120 @@
+const estados = [
+  { 
+  	sigla: 'AC', 
+  	nome: 'Acre'
+  },
+  { 
+  	sigla: 'AL',
+    nome: 'Alagoas'  
+  },
+  { 
+  	sigla: 'AP',
+    nome: 'Amapá' 
+  },
+  { 
+  	sigla: 'AM', 
+    nome: 'Amazonas'
+  },
+  { sigla: 'BA',
+    nome: 'Bahia'
+  },
+  { 
+    sigla: 'CE',
+    nome: 'Ceará' 
+    },
+  { 
+    sigla: 'DF',
+    nome: 'Distrito Federal' 
+  },
+  { 
+    sigla: 'ES',
+    nome: 'Espírito Santo' 
+  },
+  { 
+    sigla: 'GO',
+    nome: 'Goías' 
+  },
+  { 
+    sigla: 'MA',
+    nome: 'Maranhão' 
+  },
+  { 
+    sigla: 'MT',
+    nome: 'Mato Grosso' 
+  },
+  { 
+    sigla: 'MS',
+    nome: 'Mato Grosso do Sul' 
+  },
+  { 
+    sigla: 'MG',
+    nome: 'Minas Gerais' 
+  },
+  { 
+    sigla: 'PA',
+    nome: 'Pará' 
+  },
+  { 
+    sigla: 'PB',
+    nome: 'Paraíba' 
+  },
+  { 
+    sigla: 'PR',
+    nome: 'Paraná' 
+  },
+  { 
+    sigla: 'PE',
+    nome: 'Pernambuco' 
+  },
+  { 
+    sigla: 'PI',
+    nome: 'Piauí' 
+  },
+  { 
+    sigla: 'RJ',
+    nome: 'Rio de Janeiro' 
+  },
+  { 
+    sigla: 'RN',
+    nome: 'Rio Grande do Norte' 
+  },
+  { 
+    sigla: 'RS',
+    nome: 'Rio Grande do Sul' 
+  },
+  { 
+    sigla: 'RO',
+    nome: 'Rondônia' 
+  },
+  { 
+    sigla: 'RR',
+    nome: 'Roraíma' 
+  },
+  { 
+    sigla: 'SC',
+    nome: 'Santa Catarina' 
+  },
+  { 
+    sigla: 'SP',
+    nome: 'São Paulo' 
+  },
+  { 
+    sigla: 'SE',
+    nome: 'Sergipe' 
+  },
+  { 
+    sigla: 'TO',
+    nome: 'Tocantins' 
+  },
+]
+
+const UFListt = document.querySelector('#uf');
+for (let i in estados){
+  let newUF = document.createElement("option");
+  newUF.setAttribute('value', estados[i].sigla);
+  newUF.text = estados[i].nome;
+  /* newUF.innerText(estados[i].nome); */
+  console.log(estados[i].nome)
+  UFListt.appendChild(newUF);
+  
+}
