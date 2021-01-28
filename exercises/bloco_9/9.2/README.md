9cd8777da91db39c8ad64c5faa27e1c176f9442b
Rafael <rafaelgeronimo@outlook.com>
2021-01-28 19:22:14 -0300

    Dia 9.2 - Exercicio 1

---
+++ exercises/bloco_9/9.2/README.md
@@ -0,0 +1,11 @@
+# Bloco 9 | JavaScript Assíncrono e Promises
+
+## Dia 9.2 | JavaScript Promises
+
+### Exercícios
+
+1. A solução para o primeiros exercício encontra-se nesse arquivos:
+- [HTML](index.html)
+- [Script JS](apiScript.js)
+<!-- - [Página exibindo a piada](http://rafaelgeronimo.me/trybe-exercises)  pegar o endereço completo depois e adicionar aqui -->
+
---
+++ exercises/bloco_9/9.2/promise.js
@@ -0,0 +1,32 @@
+/* const promise = new Promise((resolve, reject) => {
+  const number = Math.floor(Math.random() * 11);
+
+  if (number <= 5) {
+    return reject(console.log(`Que fracasso =( Nosso número foi ${number})`));
+  }
+  resolve(console.log(`Que sucesso =) nosso número foi ${number}`));
+});
+ */
+
+/*  const promise = new Promise((resolve, reject) => {
+  const number = Math.floor(Math.random() * 11);
+
+  if (number <= 5) {
+    return reject(console.log(`Que fracasso =( Nosso número foi ${number})`));
+  }
+  resolve(number);
+ })
+ .then(number => `Que sucesso =) nosso número foi ${number}`)
+ .then(msg => console.log(msg)); */
+
+ const promise = new Promise((resolve, reject) => {
+  const number = Math.floor(Math.random() * 11);
+
+  if (number <= 5) {
+    return reject(number);
+  }
+  resolve(number);
+ })
+ .then(number => `Que sucesso =) nosso número foi ${number}`)
+ .then(msg => console.log(msg))
+ .catch(number => console.log(`Que fracasso =( Nosso número foi ${number})`));
